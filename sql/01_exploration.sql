da3efac87e21c7b8d76f69e4b8bfa30532ed5607
Simontheanalyst <smnnmsb@gmail.com>
2026-01-18 17:04:36 +0100

    Update 01_exploration.sql

--- sql/01_exploration.sql
+++ sql/01_exploration.sql
@@ -1,1 +1,17 @@
+-- Data exploration
+-- Understanding table structure and key fields
 
+SELECT *
+FROM olist_orders_dataset
+LIMIT 10;
+
+SELECT DISTINCT order_status
+FROM olist_orders_dataset;
+
+SELECT *
+FROM olist_order_items_dataset
+LIMIT 10;
+
+SELECT *
+FROM olist_customers_dataset
+LIMIT 10;
